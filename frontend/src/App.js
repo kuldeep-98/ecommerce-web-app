ed8425e363f3e4fc88f5f14c4f83c387bebcf903
kuldeep-98 <kuldeepbhama4455@gmail.com>
2020-12-22 15:41:35 +0530

    React-Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>
+        Hello World!
+      </h1>
+    </Fragment>
+  );
+}
+
+export default App;
